c73f92b452520b199fa2eaaf5572f3d727d8948a
AlexanderPlotnikov <lotos_toi@mail.ru>
2025-03-19 18:07:49 +0300

    hotfix

--- api-gateway/routes/web.php
+++ api-gateway/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('api-gateway', function(){
+    return new JsonResponse('Проверка');
 });
